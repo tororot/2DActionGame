31cfeda727ff996777d1ec452ced19712be7b54d
tororot <kamepaper0604@gmail.com>
2020-01-18 00:51:22 +0900

    2daction8
    https://dkrevel.com/makegame-beginner/make-2d-aciton-move
    8までできた

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    private Animator anim = null;
+    private Rigidbody2D rb = null;
+    public float speed;
+    float xSpeed;
+    // Start is called before the first frame update
+    void Start () {
+        anim = GetComponent<Animator> ();
+        rb = GetComponent<Rigidbody2D> ();
+        speed = 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float horizontalKey = Input.GetAxis ("Horizontal");
+
+        if (horizontalKey > 0) {
+            transform.localScale = new Vector3 (1, 1, 1);
+            anim.SetBool ("run", true);
+            xSpeed = speed;
+        } else if (horizontalKey < 0) {
+            transform.localScale = new Vector3 (-1, 1, 1);
+            anim.SetBool ("run", true);
+            xSpeed = -speed;
+        } else {
+            anim.SetBool ("run", false);
+            xSpeed = 0.0f;
+        }
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
+    }
+}
